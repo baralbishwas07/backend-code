dc845fda7dcfcc69773afc1c4e4e205617830abb
Bishwas Baral <bishwasbaral50@gmail.com>
2025-02-22 20:47:31 +0545

    solution to exercise 3.8

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
-app.use(express.json())
-app.use(morgan('tiny'))
-
 let persons = [
     { 
       id: "1",
@@ -27,6 +24,15 @@ let persons = [
       number: "39-23-6423122"
     }
 ]
+
+app.use(express.json())
+// app.use(morgan('tiny'))
+
+morgan.token('body', function(req, res) {
+    return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 const getId = () => {
     const id = String(Math.floor(Math.random() * 10000))
