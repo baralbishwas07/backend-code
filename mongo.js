c3cb361c3ec622b4ca63dd219528960ef46d1bb0
Bishwas Baral <bishwasbaral50@gmail.com>
2025-02-25 10:03:15 +0545

    solution to exercise 3.12

---
+++ mongo.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+const password = encodeURIComponent(process.argv[2])
+const name = process.argv[3]
+const number=process.argv[4]
+
+const url = `mongodb+srv://bishwasbaral50:${password}@cluster0.u53vg.mongodb.net/phoneBook?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person',personSchema)
+
+if(!name || !number){
+    console.log('phonebook:')
+    Person.find({}).then(persons => {
+        persons.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+} else {
+    const person = new Person({
+        name: name,
+        number: number
+    })
+
+    person.save().then(result => {
+        console.log(`added ${name} number ${number} to phonebook`)
+        mongoose.connection.close()
+    })
+}
