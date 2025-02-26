364b477458d74c5640b6b1be8b2aacf39f8e8de2
Bishwas Baral <bishwasbaral50@gmail.com>
2025-02-26 18:32:23 +0545

    solution to exercise 3.16

--- index.js
+++ index.js
@@ -60,17 +60,19 @@ app.get('/info', (request, response) => {
         `)
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id).then(person => {
         response.json(person)
     })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id)
     .then(result => {
         response.status(204).end()
     })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -102,6 +104,15 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
+const errorHandler = (error, request, response, next) => {
+    if(error.name === 'CastError'){
+        return response.status(400).send({error: 'malformatted id'})
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
